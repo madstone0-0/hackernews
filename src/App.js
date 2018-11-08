f8a6c632c764f3a7fe5340848c85f1b8fdb3d614
madstone0-0 <mhquansah@gmail.com>
2018-11-08 22:06:55 +0000

    Changed app.css to inline css with aphrodite

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
+import { StyleSheet, css } from "aphrodite";
 
+/* 
+*  Checks whether the term being searched is similar to any
+*  titles.
+*/
 function isSearched(searchTerm) {
   return function(item) {
     return (
@@ -9,10 +13,75 @@ function isSearched(searchTerm) {
   };
 }
 
+const stylesheet = StyleSheet.create({
+  page: {
+    margin: "20px"
+  },
+
+  interactions: {
+    textAlign: "center"
+  },
+
+  table: {
+    margin: "20px 0"
+  },
+
+  tableHeader: {
+    display: "flex",
+    lineHeight: "24px",
+    fontSize: "16px",
+    padding: "0 10px",
+    justifyContent: "space-between",
+    span: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      padding: "0 5px"
+    }
+  },
+
+  tableEmpty: {
+    margin: "200px",
+    textAlign: "center",
+    fontSize: "16px"
+  },
+
+  tableRow: {
+    display: "flex",
+    lineHeight: "24px",
+    whiteSpace: "nowrap",
+    margin: "10px 0",
+    padding: "10px",
+    background: "#ffffff",
+    border: "1px solid #e3e3e3",
+    span: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      padding: "0 5px"
+    }
+  },
+
+  buttonInline: {
+    borderWidth: "0",
+    background: "transparent",
+    color: "inherit",
+    textAlign: "inherit",
+    webkitFontSmoothing: "inherit",
+    padding: "0",
+    fontSize: "inherit",
+    cursor: "pointer"
+  },
+
+  buttonActive: {
+    borderRadius: "0",
+    borderBottom: "1px solid #38bb6c"
+  }
+});
+
 class App extends Component {
   constructor(props) {
     super(props);
 
+    // Contains table information
     const list = [
       {
         title: "React ",
@@ -32,11 +101,13 @@ class App extends Component {
       }
     ];
 
+    // Sets state
     this.state = {
       list,
       searchTerm: ""
     };
 
+    // Declare functions for global use with the this identifier
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
   }
@@ -58,8 +129,9 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="page">
-        <div className="interactions">
+      <div className={css(stylesheet.page)}>
+        <div className={css(stylesheet.interactions)}>
+          {/* Passed values form the App component to the Search an table components */}
           <Search value={searchTerm} onChange={this.onSearchChange}>
             Search
           </Search>
@@ -70,6 +142,7 @@ class App extends Component {
   }
 }
 
+// Functional stateless component Search to handle the search function
 const Search = ({ value, onChange, children }) => {
   // do something
 
@@ -81,21 +154,32 @@ const Search = ({ value, onChange, children }) => {
   );
 };
 
+const largeColumn = {
+  width: "40%"
+};
+const midColumn = {
+  width: "30%"
+};
+const smallColumn = {
+  width: "10%"
+};
+
+// Functional stateless component Table to handle the appearnce of the table
 const Table = ({ list, pattern, onDismiss }) => {
   return (
-    <div className="table">
+    <div className={css(stylesheet.table)}>
       {list.filter(isSearched(pattern)).map(item => (
-        <div key={item.objectID} className="table-row">
-          <span style={{ width: "40%" }}>
+        <div key={item.objectID} className={css(stylesheet.tableRow)}>
+          <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span style={{ width: "30%" }}>{item.author}</span>
-          <span style={{ width: "10%" }}>{item.num_comments} comments</span>
-          <span style={{ width: "10%" }}>{item.points} points</span>
-          <span style={{ width: "10%" }}>
+          <span style={midColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments} comments</span>
+          <span style={smallColumn}>{item.points} points</span>
+          <span style={smallColumn}>
             <Button
               onClick={() => onDismiss(item.objectID)}
-              className="button-inline"
+              className={css(stylesheet.buttonInline)}
             >
               Dismiss
             </Button>
