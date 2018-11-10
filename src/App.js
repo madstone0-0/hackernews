34ba904090ee720ad5fc8904b62c82076e35c78a
madstone0-0 <mhquansah@gmail.com>
2018-11-10 01:11:50 +0000

    Formatted using eslint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
 
@@ -11,202 +12,202 @@ const PARAM_SEARCH = "query=";
 *  titles.
 */
 function isSearched(searchTerm) {
-  return function(item) {
-    return (
-      !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  };
+    return function(item) {
+        return (
+            !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    };
 }
 
 const stylesheet = StyleSheet.create({
-  page: {
-    margin: "20px"
-  },
+    page: {
+        margin: "20px"
+    },
 
-  interactions: {
-    textAlign: "center"
-  },
+    interactions: {
+        textAlign: "center"
+    },
 
-  table: {
-    margin: "20px 0"
-  },
+    table: {
+        margin: "20px 0"
+    },
 
-  tableHeader: {
-    display: "flex",
-    lineHeight: "24px",
-    fontSize: "16px",
-    padding: "0 10px",
-    justifyContent: "space-between",
-    span: {
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-      padding: "0 5px"
+    tableHeader: {
+        display: "flex",
+        lineHeight: "24px",
+        fontSize: "16px",
+        padding: "0 10px",
+        justifyContent: "space-between",
+        span: {
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            padding: "0 5px"
+        }
+    },
+
+    tableEmpty: {
+        margin: "200px",
+        textAlign: "center",
+        fontSize: "16px"
+    },
+
+    tableRow: {
+        display: "flex",
+        lineHeight: "24px",
+        whiteSpace: "nowrap",
+        margin: "10px 0",
+        padding: "10px",
+        background: "#ffffff",
+        border: "1px solid #e3e3e3",
+        span: {
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            padding: "0 5px"
+        }
+    },
+
+    buttonInline: {
+        borderWidth: "0",
+        background: "transparent",
+        color: "inherit",
+        textAlign: "inherit",
+        webkitFontSmoothing: "inherit",
+        padding: "0",
+        fontSize: "inherit",
+        cursor: "pointer"
+    },
+
+    buttonActive: {
+        borderRadius: "0",
+        borderBottom: "1px solid #38bb6c"
     }
-  },
-
-  tableEmpty: {
-    margin: "200px",
-    textAlign: "center",
-    fontSize: "16px"
-  },
-
-  tableRow: {
-    display: "flex",
-    lineHeight: "24px",
-    whiteSpace: "nowrap",
-    margin: "10px 0",
-    padding: "10px",
-    background: "#ffffff",
-    border: "1px solid #e3e3e3",
-    span: {
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-      padding: "0 5px"
-    }
-  },
-
-  buttonInline: {
-    borderWidth: "0",
-    background: "transparent",
-    color: "inherit",
-    textAlign: "inherit",
-    webkitFontSmoothing: "inherit",
-    padding: "0",
-    fontSize: "inherit",
-    cursor: "pointer"
-  },
-
-  buttonActive: {
-    borderRadius: "0",
-    borderBottom: "1px solid #38bb6c"
-  }
 });
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    // Sets state
-    this.state = {
-      result: null,
-      searchTerm: DEFAULT_QUERY
-    };
+        // Sets state
+        this.state = {
+            result: null,
+            searchTerm: DEFAULT_QUERY
+        };
 
-    // Declare functions for global use with the this identifier
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.fetchTopStotries = this.fetchTopStotries.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-  }
-
-  // Set top stories
-  setSearchTopStories(result) {
-    this.setState({ result });
-  }
-
-  // Fetch the top stories
-  fetchTopStotries(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(respone => Response.json)
-      .then(result => this.setSearchTopStories(result));
-  }
-
-  // Dismiss stuff
-  onDismiss(id) {
-    const isNotId = item => {
-      return item.objectID !== id;
-    };
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
-  }
-
-  //Search stuff
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
-
-  componentDidMount() {
-    const { searchTerm } = this.state;
-    this.fetchTopStotries(searchTerm);
-  }
-
-  render() {
-    const { searchTerm, result } = this.state;
-    if (!result) {
-      return null;
+        // Declare functions for global use with the this identifier
+        this.setSearchTopStories = this.setSearchTopStories.bind(this);
+        this.fetchTopStotries = this.fetchTopStotries.bind(this);
+        this.onDismiss = this.onDismiss.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
     }
-    return (
-      <div className={css(stylesheet.page)}>
-        <div className={css(stylesheet.interactions)}>
-          {/* Passed values form the App component to the Search an table components */}
-          <Search value={searchTerm} onChange={this.onSearchChange}>
+
+    // Set top stories
+    setSearchTopStories(result) {
+        this.setState({ result });
+    }
+
+    // Fetch the top stories
+    fetchTopStotries(searchTerm) {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+            .then(respone => Response.json)
+            .then(result => this.setSearchTopStories(result));
+    }
+
+    // Dismiss stuff
+    onDismiss(id) {
+        const isNotId = item => {
+            return item.objectID !== id;
+        };
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({ list: updatedList });
+    }
+
+    //Search stuff
+    onSearchChange(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    componentDidMount() {
+        const { searchTerm } = this.state;
+        this.fetchTopStotries(searchTerm);
+    }
+
+    render() {
+        const { searchTerm, result } = this.state;
+        if (!result) {
+            return null;
+        }
+        return (
+            <div className={css(stylesheet.page)}>
+                <div className={css(stylesheet.interactions)}>
+                    {/* Passed values form the App component to the Search an table components */}
+                    <Search value={searchTerm} onChange={this.onSearchChange}>
             Search
-          </Search>
-        </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
-      </div>
-    );
-  }
+                    </Search>
+                </div>
+                <Table
+                    list={result.hits}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss}
+                />
+            </div>
+        );
+    }
 }
 
 // Functional stateless component Search to handle the search function
 const Search = ({ value, onChange, children }) => {
-  // do something
+    // do something
 
-  return (
-    <form>
-      {children}
-      <input type="text" onChange={onChange} value={value} />
-    </form>
-  );
+    return (
+        <form>
+            {children}
+            <input type="text" onChange={onChange} value={value} />
+        </form>
+    );
 };
 
 const largeColumn = {
-  width: "40%"
+    width: "40%"
 };
 const midColumn = {
-  width: "30%"
+    width: "30%"
 };
 const smallColumn = {
-  width: "10%"
+    width: "10%"
 };
 
-// Functional stateless component Table to handle the appearnce of the table
+// Functional stateless component Table to handle the appearance of the table
 const Table = ({ list, pattern, onDismiss }) => {
-  return (
-    <div className={css(stylesheet.table)}>
-      {list.filter(isSearched(pattern)).map(item => (
-        <div key={item.objectID} className={css(stylesheet.tableRow)}>
-          <span style={largeColumn}>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span style={midColumn}>{item.author}</span>
-          <span style={smallColumn}>{item.num_comments} comments</span>
-          <span style={smallColumn}>{item.points} points</span>
-          <span style={smallColumn}>
-            <Button
-              onClick={() => onDismiss(item.objectID)}
-              className={css(stylesheet.buttonInline)}
-            >
+    return (
+        <div className={css(stylesheet.table)}>
+            {list.filter(isSearched(pattern)).map(item => (
+                <div key={item.objectID} className={css(stylesheet.tableRow)}>
+                    <span style={largeColumn}>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span style={midColumn}>{item.author}</span>
+                    <span style={smallColumn}>{item.num_comments} comments</span>
+                    <span style={smallColumn}>{item.points} points</span>
+                    <span style={smallColumn}>
+                        <Button
+                            onClick={() => onDismiss(item.objectID)}
+                            className={css(stylesheet.buttonInline)}
+                        >
               Dismiss
-            </Button>
-          </span>
+                        </Button>
+                    </span>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 const Button = ({ onClick, className = "", children }) => {
-  return (
-    <button type="button" onClick={onClick} className={className}>
-      {children}
-    </button>
-  );
+    return (
+        <button type="button" onClick={onClick} className={className}>
+            {children}
+        </button>
+    );
 };
 
 export default App;
