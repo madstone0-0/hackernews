831feabe440b83ba9a61cf424d1c076e5cdee45f
madstone0-0 <mhquansah@gmail.com>
2018-11-21 06:02:07 +0000

    Edited manifest

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
 const PARAM_PAGE = "page=";
 const PARAM_HPP = "hitsPerPage=";
+const TAG = "tags=story";
 
 const stylesheet = StyleSheet.create({
     page: {
@@ -82,14 +83,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        // initializies state
+        // initializes state
         this.state = {
             result: null,
+            searchKey: "",
             searchTerm: DEFAULT_QUERY,
             error: null,
         };
 
         // Declare functions for global use with the this identifier
+        this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
         this.setSearchTopstories = this.setSearchTopstories.bind(this);
         this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
         this.onSearchSubmit = this.onSearchSubmit.bind(this);
@@ -97,38 +100,63 @@ class App extends Component {
         this.onSearchChange = this.onSearchChange.bind(this);
     }
 
+    needsToSearchTopstories(searchTerm) {
+        return !this.state.results[searchTerm];
+    }
+
     // OnSubmit function for search button enables server side searching
     onSearchSubmit(event) {
         const { searchTerm } = this.state;
-        this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+        this.setState({ searchKey: searchTerm });
+        if (this.needsToSearchTopstories(searchTerm)) {
+            this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+        }
         event.preventDefault();
     }
 
     // Set top stories
     setSearchTopstories(result) {
         const { hits, page } = result;
-        const oldHits = page !== 0 ? this.state.result.hits : [];
-        const updatedHits = [oldHits, hits];
+        const { searchKey, results } = this.state;
+        const oldHits =
+            results && results[searchKey] ? results[searchKey].hits : [];
+        const updatedHits = [...oldHits, ...hits];
 
-        this.setState({ result: { hits: updatedHits, page } });
+        this.setState({
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page },
+            },
+        });
     }
 
     // Fetch the top stories
-    fetchSearchTopstories(searchTerm, page) {
-        axios(
-            `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`,
+    fetchSearchTopstories(searchTerm, page = 0) {
+        fetch(
+            `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${TAG}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`,
         )
             .then(response => response.json())
             .then(result => this.setSearchTopstories(result))
             .catch(error => this.setState({ error }));
     }
 
+    componentDidMount() {
+        const { searchTerm } = this.state;
+        this.setState({ searchKey: searchTerm });
+        this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+    }
+
     // Dismiss stories
     onDismiss(id) {
+        const { searchKey, results } = this.state;
+        const { hits, page } = results[searchKey];
         const isNotID = item => item.objectID !== id;
-        const updatedHits = this.state.result.hits.filter(isNotID);
+        const updatedHits = hits.filter(isNotID);
         this.setState({
-            result: (this.state.result, { hits: updatedHits }),
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page },
+            },
         });
     }
 
@@ -138,12 +166,11 @@ class App extends Component {
     }
 
     render() {
-        const { searchTerm, result } = this.state;
-        const page = (result && result.page) || 0;
-        if (!result) {
-            return null;
-        }
-
+        const { searchTerm, results, searchKey } = this.state;
+        const page =
+            (results && results[searchKey] && results[searchKey].page) || 0;
+        const list =
+            (results && results[searchKey] && results[searchKey].hits) || [];
         return (
             <div className={css(stylesheet.page)}>
                 <div className={css(stylesheet.interactions)}>
@@ -156,13 +183,11 @@ class App extends Component {
                         Search
                     </Search>
                 </div>
-                {result && (
-                    <Table list={result.hits} onDismiss={this.onDismiss} />
-                )}
+                <Table list={list} onDismiss={this.onDismiss} />
                 <div className={css(stylesheet.interactions)}>
                     <Button
                         onClick={() =>
-                            this.fetchSearchTopstories(searchTerm, page + 1)
+                            this.fetchSearchTopstories(searchKey, page + 1)
                         }
                     >
                         More
@@ -171,18 +196,15 @@ class App extends Component {
             </div>
         );
     }
-
-    componentDidMount() {
-        const { searchTerm } = this.state;
-        this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
-    }
 }
 
 // Functional stateless component Search to handle the search function
 const Search = ({ value, onChange, children, onSubmit }) => (
     <form onSubmit={onSubmit}>
         <input type="text" value={value} onChange={onChange} />
-        <button type="submit"><span className="glyphicon glyphicon-search"></span> {children}</button>
+        <button type="submit">
+            <span className="glyphicon glyphicon-search" /> {children}
+        </button>
     </form>
 );
 
@@ -197,32 +219,28 @@ const smallColumn = {
 };
 
 // Functional stateless component Table to handle the appearance of the table
-const Table = ({ list, onDismiss }) => {
-    return (
-        <div className={css(stylesheet.table)}>
-            {list.map(item => (
-                <div key={item.objectID} className={css(stylesheet.tableRow)}>
-                    <span style={largeColumn}>
-                        <a href={item.url}>{item.title}</a>
-                    </span>
-                    <span style={midColumn}>{item.author}</span>
-                    <span style={smallColumn}>
-                        {item.num_comments} comments
-                    </span>
-                    <span style={smallColumn}>{item.points} points</span>
-                    <span style={smallColumn}>
-                        <Button
-                            onClick={() => onDismiss(item.objectID)}
-                            className={css(stylesheet.buttonInline)}
-                        >
-                            Dismiss
-                        </Button>
-                    </span>
-                </div>
-            ))}
-        </div>
-    );
-};
+const Table = ({ list, onDismiss }) => (
+    <div className={css(stylesheet.table)}>
+        {list.map(item => (
+            <div key={item.objectID} className={css(stylesheet.tableRow)}>
+                <span style={largeColumn}>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span style={midColumn}>{item.author}</span>
+                <span style={smallColumn}>{item.num_comments} comments</span>
+                <span style={smallColumn}>{item.points} points</span>
+                <span style={smallColumn}>
+                    <Button
+                        onClick={() => onDismiss(item.objectID)}
+                        className={css(stylesheet.buttonInline)}
+                    >
+                        Dismiss
+                    </Button>
+                </span>
+            </div>
+        ))}
+    </div>
+);
 
 const Button = ({ onClick, className = "", children }) => (
     <button type="button" onClick={onClick} className={className}>
