d752e08e156797d3d4746f8265b44bd15c164642
madstone0-0 <mhquansah@gmail.com>
2018-12-16 21:11:25 +0000

    Completed Dark theme feature

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -5,13 +5,21 @@ import { Button } from "react-bootstrap";
 import PropTypes from "prop-types";
 import "./index.css";
 
+const butonStyle = {
+    marginBottom: "5px",
+};
+
 // Search Component
 class Search extends Component {
     render() {
         const { value, onChange, onSubmit, children } = this.props;
         return (
             <div className="search">
-                <form onSubmit={onSubmit}>
+                <form
+                    className="form form-group"
+                    style={butonStyle}
+                    onSubmit={onSubmit}
+                >
                     <input type="text" value={value} onChange={onChange} />
                     <Button bsStyle="primary" type="submit">
                         {children}
